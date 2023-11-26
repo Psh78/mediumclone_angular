d2e82bfea8aca937eaec3b45124475948e40246e
Psh78 <bocha.prasanth78@gmail.com>
2023-11-26 12:59:05 +0530

    initial commit Medium clone, ngrx practice

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,19 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+// import { enableProdMode } from '@angular/core';
+// import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+import {bootstrapApplication} from '@angular/platform-browser'
+import {AppComponent} from './app/app.component'
+import {provideRouter} from '@angular/router'
+import {appRoutes} from './app/app.routes'
 
-if (environment.production) {
-  enableProdMode();
-}
+// import { AppModule } from './app/app.module';
+// import { environment } from './environments/environment';
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+// if (environment.production) {
+//   enableProdMode();
+// }
+
+// platformBrowserDynamic().bootstrapModule(AppModule)
+//   .catch(err => console.error(err));
+
+bootstrapApplication(AppComponent, {providers: [provideRouter(appRoutes)]})
